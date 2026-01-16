53601257a60f9c2078175450394b606abff14c9f
Navneet Tiwari <navneettiwari334@gmail.com>
2026-01-16 17:57:39 +0530

    oops

---
+++ Basics/oops.py
@@ -0,0 +1,14 @@
+from medium.oopsin import medium_solution
+class solution:
+    # @staticmethod
+    def make_solution(self):
+        print("the solution function")
+
+class execution:
+    sol = solution()
+    mc = medium_solution()
+    alias = medium_solution
+    al = alias()
+    al.cat()
+    sol.make_solution()
+    mc.cat()
