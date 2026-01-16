2bb6f40e6f079f0ab9912aee0030ac10c42f517b
Navneet Tiwari <navneettiwari334@gmail.com>
2026-01-16 17:58:58 +0530

    oopsin

---
+++ Basics/medium/oopsin.py
@@ -0,0 +1,15 @@
+class medium_solution:
+    def cat(self):
+        print("the function creates a cat")
+        print("the function exits now")
+
+class solution:
+    def make_solution(self):
+        print("package class method")
+
+class medium_execution:
+    pass
+
+if __name__ == "__main__":
+    msol = medium_solution()
+    msol.cat()
